658ee1f96dfeb5078030650504299d66b4f9e7a3
KanzaTahreem <106119105+KanzaTahreem@users.noreply.github.com>
2022-06-19 23:59:59 +0500

    Create script.js

---
+++ 02. quiz-app/script.js
@@ -0,0 +1,58 @@
+
+let questions = [
+  {
+  numb: 1,
+  question: "A compiler is a",
+  answer: "Machine-dependent and OS-dependent",
+  options: [
+    "Machine-independent and OS-independent",
+    "Machine-dependent and OS-dependent",
+    "Machine-dependent and OS-independent",
+    "Machine-independent and OS-dependent"
+  ]
+},
+{
+  numb: 2,
+  question: "The HTML tags are all?",
+  answer: "Not case sensitive",
+  options: [
+    "In lower case",
+    "In upper case",
+    "Case sensitive",
+    "Not case sensitive"
+  ]
+},
+{
+  numb: 3,
+  question: "Which is correct CSS syntax?",
+  answer: "body {color: black;}",
+  options: [
+    "{body;color:black;}",
+    "{body:color=black;}",
+    "body:color=black;",
+    "body {color: black;}"
+  ]
+},
+  {
+  numb: 4,
+  question: "JavaScript is an _ language?",
+  answer: "Object-Oriented",
+  options: [
+    "Object-Oriented",
+    "Object-Based",
+    "Procedural",
+    "None of the above"
+  ]
+},
+  {
+  numb: 5,
+  question: " Identify the correct statement",
+  answer: "All XML elements must be properly closed",
+  options: [
+    "All XML elements must be lower case",
+    "All XML documents may not have a DTD",
+    "All XML elements must be properly closed",
+    "All the statements are true"
+  ]
+},
+];
